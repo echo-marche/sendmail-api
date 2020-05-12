fd149f01bd6fd8f839820f8b27bb429405ed5ccb
Naotsugu Someya <chome.au.lait.github@gmail.com>
2020-05-12 09:34:57 +0900

    メール送信ロジック完了

--- servers/sendmail_server.go
+++ servers/sendmail_server.go
@@ -13,37 +13,41 @@ import (
 
 type SendmailServer struct{}
 
+type unencryptedAuth struct {
+	smtp.Auth
+}
+
+func (a unencryptedAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	s := *server
+	s.TLS = true
+	return a.Auth.Start(&s)
+}
+
 func (server *SendmailServer) SendSample(ctx context.Context, req *pb.EmailRequest) (*pb.EmailResponse, error) {
 	from := req.FromAddress
 	to := req.ToAddress
 
-	// func PlainAuth(identity, username, password, host string) Auth
-	auth := smtp.PlainAuth("", from, "secret", config.GetEnv("SMTP_HOST"))
+	auth := unencryptedAuth{smtp.PlainAuth("", from, "password", config.GetEnv("SMTP_HOST"))}
 
 	msg := []byte("" +
-		"From: 送信した人 <" + from + ">\r\n" +
+		"From: " + req.FromUserName + " <" + from + ">\r\n" +
 		"To: " + to + "\r\n" +
 		"Subject: 件名 " + req.Subject + " \r\n" +
 		"\r\n" +
 		req.Msg +
 		"\r\n")
 
-	// func SendMail(addr string, a Auth, from string, to []string, msg []byte) error
 	err := smtp.SendMail(
 		config.GetEnv("SMTP_HOST")+":"+config.GetEnv("SMTP_PORT"),
 		auth,
 		from,
 		[]string{to},
-		msg,
+		[]byte(msg),
 	)
 	if err != nil {
-		if err != nil {
-			fmt.Println(err)
-			return nil, status.Errorf(codes.Unavailable,
-				err.Error())
-		}
+		fmt.Println(err)
+		return nil, status.Errorf(codes.Unavailable,
+			err.Error())
 	}
-
-	fmt.Print("success")
 	return &pb.EmailResponse{Status: "sendmail ok"}, nil
 }
